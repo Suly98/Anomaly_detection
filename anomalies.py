5b4c69d763e19f9e65dfbe9cc4bacbfd9c8a4e50
sulaiman alhudaifi <alhinaa@uni.edu>
2024-10-04 09:16:43 +0400

    added colors in current_plot()

--- anomalies.py
+++ anomalies.py
@@ -5,9 +5,9 @@ import numpy as np
 from sklearn.ensemble import IsolationForest
 import matplotlib.pyplot as plt
 
-def generate_data(size = 100):
+def generate_data(size = 1000):
     normal_data = np.random.normal(0, 1, size)
-    anomalies = np.random.uniform(10, 15, size//10)
+    anomalies = np.random.uniform(10, 15, size//100)
     data_stream = np.concatenate((normal_data, anomalies))
     np.random.shuffle(data_stream)
     return data_stream
@@ -18,31 +18,31 @@ def anomalies_detector(data_stream):
     predictions = isolate_forest.fit_predict(data)
     return predictions
 
-
-def simulate():
-    stream = generate_data()
-    for point in stream:
-        predictions = anomalies_detector([point])
-        print(f"Data: {point}, Anomaly: {predictions[0] == -1}")
-
+def simulate(stream):
+    predictions = anomalies_detector(stream)
+    for i,point in enumerate(stream):
+        print(f"Data: {point}, Anomaly: {predictions[i] == -1}")
 
 def current_plot(stream):
     plt.ion()
     fig, ax = plt.subplots()
     data = []
-    for point in stream:
+
+    for i,point in enumerate(stream):
         data.append(point)
         ax.clear()
-        ax.plot(data, label='Data Stream')
-        ax.scatter(len(data)-1, point, color='red' if anomalies_detector([point])[0] == -1 else 'green')
-        plt.pause(0.1)
+        ax.plot(data, label='Data Stream', color='blue')
+
+        if predictions[i] == -1:
+            ax.scatter(len(data) -1 , point, color='red', label='Anamoly')
+        plt.pause(0.08)
     plt.show()
 
 def main():
     stream = generate_data()
 
     print("starting simulation...")
-    simulate()
+    simulate(stream)
 
     print("starting visualization...")
     current_plot(stream)
