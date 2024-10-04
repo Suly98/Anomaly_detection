2fbd44ca04b1848b2673d28bfc94252d563f29db
sulaiman alhudaifi <alhinaa@uni.edu>
2024-10-04 08:51:38 +0400

    fixed code

--- anomalies.py
+++ anomalies.py
@@ -2,34 +2,29 @@
 ## this file is a python script that will detect anomalies in a coninues data stream!
 
 import numpy as np
+from sklearn.ensemble import IsolationForest
+import matplotlib.pyplot as plt
 
-def generate_data(size = 1000):
+def generate_data(size = 100):
     normal_data = np.random.normal(0, 1, size)
-    anomalies = np.random.uniform(10, 15, size//100)
-    data_stream = np.concatenate(normal_data, anomalies)
+    anomalies = np.random.uniform(10, 15, size//10)
+    data_stream = np.concatenate((normal_data, anomalies))
     np.random.shuffle(data_stream)
-
     return data_stream
 
-from sklearn.ensemble import IsolationForest
-
 def anomalies_detector(data_stream):
-    isolate_forest = IsolationForest(contamination = 0.01)
+    isolate_forest = IsolationForest(contamination = 0.1)
     data = np.array(data_stream).reshape(-1,1)
     predictions = isolate_forest.fit_predict(data)
     return predictions
 
-import time
 
 def simulate():
     stream = generate_data()
     for point in stream:
         predictions = anomalies_detector([point])
         print(f"Data: {point}, Anomaly: {predictions[0] == -1}")
-        time.sleep(0.1)
 
-
-import matplotlib.pyplot as plt
 
 def current_plot(stream):
     plt.ion()
@@ -50,7 +45,7 @@ def main():
     simulate()
 
     print("starting visualization...")
-    current_plot()
+    current_plot(stream)
 
 if __name__ == "__main__":
     main()
