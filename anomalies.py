8f024ead3d33d12cfbf1dc082320c570d7599fe8
sulaiman alhudaifi <alhinaa@uni.edu>
2024-10-06 16:19:44 +0400

    added a test_anomalies.py

--- anomalies.py
+++ anomalies.py
@@ -9,30 +9,50 @@ from sklearn.ensemble import IsolationForest
 import matplotlib.pyplot as plt
 
 
-# func to generate data and outliers
 def generate_data(size = 1000):
+    """
+    Generates data stream with inliers and outliers values
+
+    args: size - total number of data points to generate.
+    Returns: data_stream - a shuffled array of normal and anomalous data.
+    """
     normal_data = np.random.normal(0, 1, size)  # generating normal data
     anomalies = np.random.uniform(10, 15, size//100) #generatin anomaly data
     data_stream = np.concatenate((normal_data, anomalies)) # mixing them together
     np.random.shuffle(data_stream) # shuffling normal data with anomaly data
     return data_stream
 
-# func to detect anomalies
 def anomalies_detector(data_stream):
+    """
+    Detects anomalies in the data stream given using Isolation Forest.
+
+    Args: data_stream - a shuffled array of normal and anomalous data points.
+    Returns: predictions - a shuffled array 1,-1 where -1 are anomalous data. 
+    """
     isolate_forest = IsolationForest(contamination = 0.1) #starting the algorithm with %10 prediction in having anomalies
     data = np.array(data_stream).reshape(-1,1) #reshaping data so algorithm defines outliers
     predictions = isolate_forest.fit_predict(data) #making the algorithm work
     return predictions
 
-# func to check if data is outliers or inliers and printing them to mimc real time detection.
 def simulate(stream):
+    """
+    Simulates real-time data stream processing and prints detected anomalies.
+
+    Args: stream - list/array of the data stream to analyze.
+    Return: None
+    """
     predictions = anomalies_detector(stream)
     for i,point in enumerate(stream):
         print(f"Data: {point}, Anomaly: {predictions[i] == -1}")
         time.sleep(0.001)
 
-# func to visualize the data in real time.
 def current_plot(stream):
+    """
+    Visualize the data stream with outliers marked in real time.
+
+    Args: stream - The data stream to plot
+    Reeurns: None
+    """
     predictions = anomalies_detector(stream) 
     plt.ion() #adding every update while the script is running
     fig, ax = plt.subplots() #create figure with axes
@@ -56,8 +76,14 @@ def current_plot(stream):
     ax.legend() # displaying the label in reference to the blue line
     plt.show() #showing figure
 
-# main function to call all function and run the project.
 def main():
+    """
+    Main function to generate, simulate and visualize the data stream.
+
+    Args: None
+    Returns: None
+    """
+
     stream = generate_data()
 
     print("starting simulation...")
